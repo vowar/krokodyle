67bf8b615557cf833a3661079bfe0ca8b489ac81
Vladimir Parhomenko <prhvladimir@gmail.com>
2017-11-14 22:36:32 +0200

    begin

---
+++ paint.js
@@ -0,0 +1,132 @@
+var canvas = document.getElementById('canv');
+
+ctx = canvas.getContext('2d');
+line = document.getElementById('line')
+
+
+// canvas.onclick = function(){
+//     var lin = {
+//         x: event.pageX,
+//         y: event.pageY,
+//         fill: function(c){ ctx.strokeStyle = c},
+//         widthe: function(w){ctx.lineWidth = w},
+//         draw: function(color, wid){
+//             this.fill(color);
+//             this.widthe(wid)
+//             ctx.moveTo(this.x,this.y)
+//             ctx.lineTo(100, 100)
+//             ctx.stroke()
+//         }
+
+
+//     }/* lin end */
+//     lin.draw('green', line.value)
+// }
+
+
+
+// var line = {
+//     x: event.pageX;
+//     y: event.pageY;
+
+// }
+function getCoords(elem) { // кроме IE8-
+    var box = elem.getBoundingClientRect();
+  
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
+  
+  }
+
+  document.onmousemove = function(){
+      if(event.pageY< getCoords(canvas))
+  }
+
+// canvas.onmouseenter = function () {
+
+//     pointer.style.width = "50px";
+//     pointer.style.height = "50px";
+//     pointer.style.position = "absolute";
+//     pointer.style.left = `${event.pageX - pointer.clientWidth / 2}px`;
+//     pointer.style.top = `${event.pageY - pointer.clientHeight / 2}px`;
+//     pointer.style.backgroundColor = `red`;
+//     pointer.style.zIndex = `100`;
+
+//     canvas.onmousemove = function () {
+//         //console.log(event.target)
+//         pointer.style.display = "block";
+//         pointer.style.left = `${event.pageX - pointer.clientWidth / 2}px`;
+//         pointer.style.top = `${event.pageY - pointer.clientHeight / 2}px`;
+//     }
+//     pointer.onmousemove = function () {
+//         pointer.style.display = "block";
+//         pointer.style.left = `${event.pageX - pointer.clientWidth / 2}px`;
+//         pointer.style.top = `${event.pageY - pointer.clientHeight / 2}px`;
+//     }
+//     canvas.onmouseleave = function () {
+//         pointer.style.display = "none";
+//     }
+// }
+
+
+canvas.onclick = function () {
+
+    var circl = {
+        x: event.pageX,
+        y: event.pageY,
+        fill: function (c) { ctx.fillStyle = c },
+        draw: function (color, circle_width) {
+            this.fill(color);
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, circle_width, 0, Math.PI * 2);
+            ctx.stroke();
+
+            ctx.fill();
+        },
+        // touch: function (x, y) {
+        //     console.log(event.clientY)
+        //     console.log(event.pageY)
+        //     var isPath = ctx.isPointInPath(x, y);
+        //     if (isPath) {
+        //         console.log(this);
+        //         this.draw('red')
+        //     }
+        // }
+
+    }
+    colors = ['green', "red", "blue", "yellow"]
+    circl.draw(document.getElementById("color_inp").value, document.getElementById("line").value)
+}
+
+
+//  flag = true;
+
+// canvas.onmousedown = function () {
+//     flag = true
+//     var x = event.pageX;
+//     var y = event.pageY;
+
+//     console.log(x + ":" + y)
+
+//     ctx.moveTo(x, y);
+
+//     canvas.onmousemove = function () {
+//         if (flag) {
+
+//         ctx.lineWidth = document.getElementById("line").value;
+//         var mx = event.pageX
+//         var my = event.pageY
+//         ctx.lineTo(mx, my);
+
+//             ctx.stroke();
+//         }
+//         console.log("move " + mx + ":" + my)
+//     }
+//     canvas.onmouseup = function () {
+//         flag = false
+
+//     }
+
+// }   

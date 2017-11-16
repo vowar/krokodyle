3ce63325709a5fc1d0e4f3eb09a7bf9df448ec23
Vladimir Parhomenko <prhvladimir@gmail.com>
2017-11-17 00:08:54 +0200

    paint done

--- paint.js
+++ paint.js
@@ -3,80 +3,58 @@ var canvas = document.getElementById('canv');
 ctx = canvas.getContext('2d');
 line = document.getElementById('line')
 
-
-// canvas.onclick = function(){
-//     var lin = {
-//         x: event.pageX,
-//         y: event.pageY,
-//         fill: function(c){ ctx.strokeStyle = c},
-//         widthe: function(w){ctx.lineWidth = w},
-//         draw: function(color, wid){
-//             this.fill(color);
-//             this.widthe(wid)
-//             ctx.moveTo(this.x,this.y)
-//             ctx.lineTo(100, 100)
-//             ctx.stroke()
-//         }
-
-
-//     }/* lin end */
-//     lin.draw('green', line.value)
-// }
-
-
-
-// var line = {
-//     x: event.pageX;
-//     y: event.pageY;
-
-// }
 function getCoords(elem) { // кроме IE8-
     var box = elem.getBoundingClientRect();
-  
-    return {
-      top: box.top + pageYOffset,
-      left: box.left + pageXOffset
-    };
-  
-  }
 
-  document.onmousemove = function(){
-      if(event.pageY< getCoords(canvas).top + canvas.clientHeight && event.pageY >getCoords(canvas).top && event.pageX >getCoords(canvas).left && event.pageX <  getCoords(canvas).left + canvas.clientWidth){
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+
+}
+
+document.onmousemove = function () {
+    if (event.pageY < getCoords(canvas).top + canvas.clientHeight && event.pageY > getCoords(canvas).top && event.pageX > getCoords(canvas).left && event.pageX < getCoords(canvas).left + canvas.clientWidth) {
         console.log()
         pointer.style.width = `${document.getElementById('line').value}px`;
         pointer.style.height = `${document.getElementById('line').value}px`;
         pointer.style.display = "block";
         pointer.style.left = `${event.pageX - pointer.clientWidth / 2}px`;
         pointer.style.top = `${event.pageY - pointer.clientHeight / 2}px`;
-      }else{
+    } else {
         pointer.style.display = "none";
-        
-      }
-  }
 
-canvas.onmousedown = function(){
-    
-    var line ={
+    }
+}
+
+canvas.onmousedown = function () {
+    ctx.beginPath()
+    console.log(event)
+    var line = {
         x: event.pageX,
         y: event.pageY,
-        move: function(){ctx.moveTo(this.x, this.y)},
-        fill: function (c) { ctx.strokeStyle = c },
-        draw: function(){
+        move: function () { ctx.moveTo(this.x, this.y) },
+        style: function (c) { ctx.strokeStyle = c },
+        draw: function () {
             this.move()
             canvas.onmousemove = () => {
-                //console.log(this)
-                this.fill(document.getElementById("color_inp").value)
+                this.style(document.getElementById("color_inp").value)
                 var mx = event.pageX
                 var my = event.pageY
-                
+                //console.log(mx, my)
                 ctx.lineWidth = document.getElementById("line").value;
                 ctx.lineTo(mx, my);
-                ctx.stroke();
-                canvas.onmouseup = ()=>{
-                    canvas.onmousemove = null;
-                }
+                ctx.lineCap = "round";
+                ctx.lineJoin = 'round';
+                ctx.stroke();                
+                
             }
-        } 
+            canvas.onmouseup = () => {
+                canvas.onmousemove = null;
+
+                ctx.closePath();
+            }
+        }
     }
     //console.log(this)
     line.draw()
@@ -92,7 +70,7 @@ canvas.onclick = function () {
             //console.log(this)
             this.fill(color);
             ctx.beginPath();
-            ctx.arc(this.x, this.y, circle_width/2, 0, Math.PI * 2);
+            ctx.arc(this.x, this.y, circle_width / 2, 0, Math.PI * 2);
             //ctx.stroke();
 
             ctx.fill();
@@ -102,46 +80,3 @@ canvas.onclick = function () {
     }
     circl.draw(document.getElementById("color_inp").value, document.getElementById("line").value)
 }
-
-
-//  flag = true;
-
-// canvas.onmousedown = function () {
-//     flag = true
-//     var x = event.pageX;
-//     var y = event.pageY;
-
-//     console.log(x + ":" + y)
-
-//     ctx.moveTo(x, y);
-
-//     canvas.onmousemove = function () {
-//         if (flag) {
-
-//         ctx.lineWidth = document.getElementById("line").value;
-//         var mx = event.pageX
-//         var my = event.pageY
-//         ctx.lineTo(mx, my);
-
-//             ctx.stroke();
-//         }
-//         console.log("move " + mx + ":" + my)
-//     }
-//     canvas.onmouseup = function () {
-//         flag = false
-
-//     }
-
-// }   
-
-
-
-        // touch: function (x, y) {
-        //     console.log(event.clientY)
-        //     console.log(event.pageY)
-        //     var isPath = ctx.isPointInPath(x, y);
-        //     if (isPath) {
-        //         console.log(this);
-        //         this.draw('red')
-        //     }
-        // }

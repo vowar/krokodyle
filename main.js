72ed32bd7d831c943f3f0f495921530aa1857ea2
Vladimir Parhomenko <prhvladimir@gmail.com>
2017-11-23 00:42:25 +0200

    paint done

--- main.js
+++ main.js
@@ -1,70 +1,89 @@
 var players = []; 
-    document.addEventListener("DOMContentLoaded", function(){
-        enterName()
-        function enterName(){
-            var nick = prompt("Введите никнейм", "");
-            if(nick != "" || nick === null){
-                var obj = {
-                    name: nick,
-                    wins: 0
-                }
-                players = [...players, obj]
-                beginGame()
-                //console.log(players)
-            }else {
-                enterName()
+document.addEventListener("DOMContentLoaded", function(){
+    enterName()
+    function enterName(){
+        var nick = prompt("Введите никнейм", "");
+        if(nick != "" || nick != null){
+            var obj = {
+                name: nick,
+                drawer: false,
+                wins: 0
             }
+            players = [...players, obj]
+            beginGame()
+            //console.log(players)
+        }else {
+            enterName()
         }
-    });
+    }
+});
 var words = [
-        {name: "яблоко", type: "существительное"},
-        {name: "золото", type: "существительное"},
-        {name: "дождь", type: "существительное"},
-        {name: "светить", type: "глагол"},
-        {name: "гореть", type: "глагол"},
-        {name: "плакать", type: "глагол"},
-    ];
+    {name: "яблоко", type: "существительное"},
+    {name: "золото", type: "существительное"},
+    {name: "дождь", type: "существительное"},
+    {name: "светить", type: "глагол"},
+    {name: "гореть", type: "глагол"},
+    {name: "плакать", type: "глагол"},
+    {name: "острый", type: "прилагательное"},
+    {name: "открытый", type: "прилагательное"},
+    {name: "волшебный", type: "прилагательное"},
+    {name: "удивленный", type: "прилагательное"},
+];
 
 var chose_panel = document.getElementById("chose-word");
 var buttons = document.getElementById("chose_buttons");
 var chose_words = [1, 2, 3];
 
-function beginGame(){
-    var rand_words = words;
-    shuffle(rand_words)
-    chose_panel.style.visibility = "";
-    chose_panel.style.opacity = "1";
-    buttons.innerHTML = "";
-    buttons.innerHTML = `
-        <button class="chose_btn" data="${rand_words[0].type}">${rand_words[0].name}</button>
-        <button class="chose_btn" data="${rand_words[1].type}">${rand_words[1].name}</button>
-        <button class="chose_btn" data="${rand_words[2].type}">${rand_words[2].name}</button>`;
-    chose_panel.appendChild(buttons)
-}
 
+
+function beginGame(){
+
+
+document.getElementById("word-name").innerHTML = `Нарисуйте слово: `;
+document.getElementById("inner_type").innerHTML = "";
+var rand_words = words;
+shuffle(rand_words)
+chose_panel.style.visibility = "";
+chose_panel.style.opacity = "1";
+buttons.innerHTML = "";
+buttons.innerHTML = `
+    <button class="chose_btn" data="${rand_words[0].type}">${rand_words[0].name}</button>
+    <button class="chose_btn" data="${rand_words[1].type}">${rand_words[1].name}</button>
+    <button class="chose_btn" data="${rand_words[2].type}">${rand_words[2].name}</button>`;
+chose_panel.appendChild(buttons)
+}
+var time = "";
 chose_panel.onclick = function(){
-    if(event.target.className == "chose_btn"){
-        document.getElementById("word-name").innerHTML = `Нарисуйте слово: <b>"${event.target.innerHTML.toUpperCase()}"</b>`;
-        document.getElementById("inner_type").innerHTML = event.target.getAttribute("data");
-        if(event.target.getAttribute("data")=='глагол'){
-            document.getElementById("word-type").style.backgroundColor = "rgb(236,105,109)";
-            
-        }else if(event.target.getAttribute("data")=="прилагательное"){
-            document.getElementById("word-type").style.backgroundColor = "rgb(51, 191, 255)";
-        }else{
-            document.getElementById("word-type").style.backgroundColor = "rgb(71, 175, 71)";
-            
-            
-        }
-        chose_panel.style.visibility = "hidden";
-        chose_panel.style.opacity = "0";
+if(event.target.className == "chose_btn"){
+    clearInterval(time)
+    var i =300;
+    time = setInterval(function(){
+        document.getElementById("timer").innerHTML= `${i--} сек.`;
+
+    },1000)
+
+    document.getElementById("word-name").innerHTML = `Нарисуйте слово: <b>"${event.target.innerHTML.toUpperCase()}"</b>`;
+    document.getElementById("inner_type").innerHTML = event.target.getAttribute("data");
+    if(event.target.getAttribute("data")=='глагол'){
+        document.getElementById("word-type").style.backgroundColor = "rgb(236,105,109)";
+        
+    }else if(event.target.getAttribute("data")=="прилагательное"){
+        document.getElementById("word-type").style.backgroundColor = "rgb(51, 191, 255)";
+    }else{
+        document.getElementById("word-type").style.backgroundColor = "rgb(71, 175, 71)";
+        
+        
     }
+    chose_panel.style.visibility = "hidden";
+    chose_panel.style.opacity = "0";
+}
 }
 
 document.getElementById('inner_type').onclick = function(){
-    beginGame()
-    
+beginGame()
+
 }
+
 
 
 var line = document.getElementById("line");
@@ -79,17 +98,17 @@ example.style.marginLeft = `${40 - line.value/2}px`
 example.style.marginTop = `${40 - line.value/2}px`
 
 line.oninput  = function(){
-    example.style.width = `${line.value}px`
-    example.style.height = `${line.value}px`
-    example.style.marginLeft = `${40 - line.value/2}px`
-    example.style.marginTop = `${40 - line.value/2}px`
-    
+example.style.width = `${line.value}px`
+example.style.height = `${line.value}px`
+example.style.marginLeft = `${40 - line.value/2}px`
+example.style.marginTop = `${40 - line.value/2}px`
+
 }
 color.onchange = function(){
-    example.style.backgroundColor = color.value
+example.style.backgroundColor = color.value
 }
 color_background.onchange = function(){
-    canvas.style.backgroundColor = color_background.value
+canvas.style.backgroundColor = color_background.value
 }
 
 var color_line = document.getElementById("chose-colors")
@@ -97,29 +116,29 @@ console.log(color_line)
 
 var color_arr = ["rgb(0,0,0)", "rgb(255,255,255)", "#a5a5a5", "#ffd0e2", "#33bfff", "#1c1cff", "#01c622", "#f7e601", "#ffe5b2", "#ff8e00", "#ff0b00", "#6f2c0b"]
 for(var i = 0; i <12; i++){
-    var block = document.createElement("div");
-    block.classList.add("color-item");
-    block.style.backgroundColor = `${color_arr[i]}`
-    color_line.appendChild(block)
+var block = document.createElement("div");
+block.classList.add("color-item");
+block.style.backgroundColor = `${color_arr[i]}`
+color_line.appendChild(block)
 }
 color_line.onclick = () => {
-    if(event.target.className == "color-item"){
-        color.value = `#${getHexRGBColor(event.target.style.backgroundColor)}`
-        example.style.backgroundColor = `${event.target.style.backgroundColor}`
-    }
+if(event.target.className == "color-item"){
+    color.value = `#${getHexRGBColor(event.target.style.backgroundColor)}`
+    example.style.backgroundColor = `${event.target.style.backgroundColor}`
+}
 }
 var send_answer = document.getElementById("answer-btn");
 var answer_inp = document.getElementById("answer");
 send_answer.onclick = function(){
-    if(answer_inp.value != ""){
-        var variant = document.createElement("p");
-        variant.className = "variant";
-        variant.innerHTML = `
-            <span class="variant-author">${players[0].name}:</span>
-            <span class="main-text">${answer_inp.value}</span>`
-        document.getElementById("variants-list").appendChild(variant)
-        answer_inp.value = "";
-    }
+if(answer_inp.value != ""){
+    var variant = document.createElement("p");
+    variant.className = "variant";
+    variant.innerHTML = `
+        <span class="variant-author">${players[0].name}:</span>
+        <span class="main-text">${answer_inp.value}</span>`
+    document.getElementById("variants-list").appendChild(variant)
+    answer_inp.value = "";
+}
 }
 
 
@@ -144,26 +163,25 @@ send_answer.onclick = function(){
 
 
 function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
+for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+}
 }
 
 
 function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getHexRGBColor(color)
 {
-  color = color.replace(/\s/g,"");
-  var aRGB = color.match(/^rgb\((\d{1,3}[%]?),(\d{1,3}[%]?),(\d{1,3}[%]?)\)$/i);
-  if(aRGB){
-    color = "";
-    for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,"0$1");
-  }
-  else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, "$1$1$2$2$3$3");
-  return color;
+color = color.replace(/\s/g,"");
+var aRGB = color.match(/^rgb\((\d{1,3}[%]?),(\d{1,3}[%]?),(\d{1,3}[%]?)\)$/i);
+if(aRGB){
+color = "";
+for (var i=1;  i<=3; i++) color += Math.round((aRGB[i][aRGB[i].length-1]=="%"?2.55:1)*parseInt(aRGB[i])).toString(16).replace(/^(.)$/,"0$1");
 }
-
+else color = color.replace(/^#?([\da-f])([\da-f])([\da-f])$/i, "$1$1$2$2$3$3");
+return color;
+}
--- paint.js
+++ paint.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('canv');
 
 ctx = canvas.getContext('2d');
-var line = document.getElementById('line');
+var line_w = document.getElementById("line");
 var color = document.getElementById('color_inp');
 
 
@@ -16,11 +16,11 @@ function getCoords(elem) { // кроме IE8-
 }
 // Visual effects
 
-document.onmousemove = function () {
+window.onmousemove = function () {
     if (event.target != document.getElementById('chose-word') && event.pageY < getCoords(canvas).top + canvas.clientHeight && event.pageY > getCoords(canvas).top && event.pageX > getCoords(canvas).left && event.pageX < getCoords(canvas).left + canvas.clientWidth) {
         console.log()
-        pointer.style.width = `${line.value}px`;
-        pointer.style.height = `${line.value}px`;
+        pointer.style.width = `${line_w.value}px`;
+        pointer.style.height = `${line_w.value}px`;
         pointer.style.display = "block";
         pointer.style.left = `${event.pageX - pointer.clientWidth / 2}px`;
         pointer.style.top = `${event.pageY - pointer.clientHeight / 2}px`;
@@ -30,10 +30,43 @@ document.onmousemove = function () {
     }
 }
 
-document.getElementById('clear-canvas').onclick = function(){
+
+
+document.getElementById('clear-canvas').onclick = function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 // Draw logic
+var coord_array = [];
+document.getElementById("back-event").onclick = () => {
+    coord_array.pop();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    
+    for (var i = 0; i < coord_array.length; i++) {
+
+        if (coord_array[i].move_coord.length !== 0) {
+            ctx.beginPath();
+            ctx.lineWidth = coord_array[i].width;
+            ctx.strokeStyle = coord_array[i].colr;
+            ctx.moveTo(coord_array[i].startX,coord_array[i].startY);
+            for(var j = 0;j<coord_array[i].move_coord.length;j++){
+                ctx.lineTo(coord_array[i].move_coord[j].x,coord_array[i].move_coord[j].y)
+            }
+            ctx.stroke(); 
+            
+        } else {
+            ctx.beginPath();
+            ctx.arc(coord_array[i].startX, coord_array[i].startY, coord_array[i].width /2, 0, Math.PI * 2);
+            ctx.fillStyle = coord_array[i].colr;
+            ctx.fill();
+        }
+
+         
+    }
+    console.log(coord_array)
+}
+var obj ="";
+
 canvas.onmousedown = function () {
     ctx.beginPath()
     //console.log(event)
@@ -44,12 +77,20 @@ canvas.onmousedown = function () {
         style: function (c) { ctx.strokeStyle = c },
         draw: function () {
             this.move()
-            canvas.onmousemove = () => {
+            var obj = {
+            	startX: this.x,
+                startY: this.y,
+                width: line_w.value,
+                colr: color.value,
+            	move_coord: [],
+            }
+            //console.log(obj)
+            document.onmousemove = () => {
                 this.style(color.value)
                 var mx = event.pageX - canvas.offsetLeft;
                 var my = event.pageY - canvas.offsetTop;
-                //console.log(mx, my)
-                ctx.lineWidth = document.getElementById("line").value;
+                obj = {...obj, move_coord: [...obj.move_coord, {x: mx, y: my}]}
+                ctx.lineWidth = line_w.value;
                 ctx.lineTo(mx, my);
                 ctx.lineCap = "round";
                 // ctx.shadowBlur=1;
@@ -58,16 +99,19 @@ canvas.onmousedown = function () {
                 ctx.stroke();
                 
             }
-            canvas.onmouseup = () => {
-                canvas.onmousemove = null;
-
+            document.onmouseup = () => {
+                document.onmousemove = null;
+                coord_array = [...coord_array, obj];
+                console.log(coord_array);
                 ctx.closePath();
+                document.onmouseup = null;
             }
         }
     }
     //console.log(this)
     line.draw()
 }
+
 
 
 canvas.onclick = function () {
@@ -81,11 +125,11 @@ canvas.onclick = function () {
             ctx.beginPath();
             ctx.arc(this.x, this.y, circle_width / 2, 0, Math.PI * 2);
             //ctx.stroke();
-
+            ctx.closePath()
             ctx.fill();
         },
 
 
     }
-    circl.draw(document.getElementById("color_inp").value, document.getElementById("line").value)
+    circl.draw(document.getElementById("color_inp").value, line_w.value)
 }
